8c1bbb785e22e947ff4b3296372a7109f89a3fb0
rtjshreyd <shreyanshece1041@gmail.com>
2021-07-08 14:44:41 +0530

    Mymessage and TheirMessage apperaing fine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { ChatEngine } from 'react-chat-engine';
-
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
     return (
         <ChatEngine
-            height="100vh"
-            projectID="0c358338-add6-4d73-859b-07aee3f0a2db"
-            userName="shreyanshdube"
-            userSecret="123456789"
+            height = "100vh"
+            projectID = "0c358338-add6-4d73-859b-07aee3f0a2db"
+            userName = "shreyanshdube"
+            userSecret = "123456789"
+            renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} /> }
         />
     )
 };
